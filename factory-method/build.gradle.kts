521c3e08ca809875ffff91d6106d277a3df8d453
Mateo Curicho <m_curicho@buzz-ly.com>
2025-06-01 20:22:59 -0500

    git guava dependecy added

--- factory-method/build.gradle.kts
+++ factory-method/build.gradle.kts
@@ -12,6 +12,9 @@ repositories {
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+
+    // https://mvnrepository.com/artifact/com.google.guava/guava
+    implementation("com.google.guava:guava:33.4.8-jre")
 }
 
 tasks.test {
